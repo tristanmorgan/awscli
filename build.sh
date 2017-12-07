589a10b70acee9c509d1a5c83cab5f73a3351cb4
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2017-12-07 17:06:50 +1100

    tag it with my docker hub account.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 export AWS_CLI_VERSION=$1
 
-docker build --build-arg AWS_CLI_VERSION=$AWS_CLI_VERSION -t awscli:latest -t awscli:$AWS_CLI_VERSION .
+docker build --build-arg AWS_CLI_VERSION=$AWS_CLI_VERSION -t tristanmorgan/awscli:latest -t tristanmorgan/awscli:$AWS_CLI_VERSION .
