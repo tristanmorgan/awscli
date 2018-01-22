a5c476a5cc12c3b3c12db63fa75b1ff50a4318d9
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2018-01-23 08:50:28 +1100

    push it, push it real good.

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 export AWS_CLI_VERSION=$1
 
 docker build --build-arg AWS_CLI_VERSION=$AWS_CLI_VERSION -t tristanmorgan/awscli:latest -t tristanmorgan/awscli:$AWS_CLI_VERSION .
+docker push tristanmorgan/awscli:latest
+docker push tristanmorgan/awscli:$AWS_CLI_VERSION
