ae65e085e4b6cf87a959e2c2987c30b24642129c
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2018-06-04 11:11:02 +1000

    Update pip and don't use cache.

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -e
 export AWS_CLI_VERSION=$1
 
-docker build --build-arg AWS_CLI_VERSION=$AWS_CLI_VERSION -t tristanmorgan/awscli:latest -t tristanmorgan/awscli:$AWS_CLI_VERSION .
+docker build  --no-cache --build-arg AWS_CLI_VERSION=$AWS_CLI_VERSION -t tristanmorgan/awscli:latest -t tristanmorgan/awscli:$AWS_CLI_VERSION .
 docker push tristanmorgan/awscli:latest
 docker push tristanmorgan/awscli:$AWS_CLI_VERSION
