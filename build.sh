1060127b87fc09c08d7e0650a591bbbe20577270
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2020-10-16 11:36:50 +1100

    tag latest.

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 set -e
 export AWS_CLI_VERSION=$1
 
-docker build --pull --no-cache -t tristanmorgan/awscli:$AWS_CLI_VERSION .
+docker build --pull --no-cache -t tristanmorgan/awscli:latest -t tristanmorgan/awscli:$AWS_CLI_VERSION .
 docker push tristanmorgan/awscli:$AWS_CLI_VERSION
+docker push tristanmorgan/awscli:latest
